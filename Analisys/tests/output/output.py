b4e76594bb9f47678b12c5c8fc227cea2748e774
RisaMagpie <risahawkeye@mail.ru>
2018-11-19 23:15:37 +0300

    Update output.py

--- Analisys/tests/output/output.py
+++ Analisys/tests/output/output.py
@@ -9,7 +9,6 @@ def output(res, courseName):
     res=res.drop(columns=['problem_id', 'page'])
     res=res.groupby(['course_name'])['questions'].apply(','.join).reset_index()
     res['questions']='{"course":'+res['course_name']+',"tests":['+res['questions']+']'
-    course_name=(res['course_name'][0]+ '_test_statistics.json').replace('+', '_').split(':')[1]
     res=res.drop(columns=['course_name'])
     fileName = courseName + '_tests_statistics.json'
     data_file = open(course_name, 'w')
