6dc5d5ffd8cf167c40699c0d144538222d5ce1e6
Svetlana <risahawkeye@mail.ru>
2018-11-27 22:40:35 +0300

    new output with new fields

--- Analisys/tests/content/test_questions_analisys.py
+++ Analisys/tests/content/test_questions_analisys.py
@@ -4,19 +4,19 @@ import numpy as np
 def calculateQuestions(listForQuestions):
 
     #calculating: sum of grades on question and number of answers on question
-    perc=listForQuestions.groupby(['course_name','problem_id','page', 'attempts','max_grade','question'])['result'].agg([np.sum , np.size])
+    perc=listForQuestions.groupby(['course_name','page','problem_type','problem_id','attempts','question'])['result'].agg([np.sum , np.size])
 
     #calcilating percentage of right answers on each question
     perc=100*perc['sum']/perc['size']
     perc=perc.reset_index()
 
     #merge questions and percentage of right answers
-    perc['questions'] = perc['question'].map(str) +':' +perc[0].map(str)
+    perc['questions'] = '{"question":'+perc['question'].map(str) +',"percent_of_right_answers":' +perc[0].map(str)+'}'
     perc=perc.drop(columns=['question', 0])
 
     #merge questions in test
-    perc=perc.groupby(['course_name','problem_id','page','attempts','max_grade'])['questions'].apply(','.join).reset_index()
-    perc['questions']='{ ' + perc['questions'] +'}'
+    perc=perc.groupby(['course_name','page','problem_type','problem_id','attempts'])['questions'].apply(','.join).reset_index()
+    perc['questions']='[' + perc['questions'] +']'
 
     return perc
 
--- Analisys/tests/main.py
+++ Analisys/tests/main.py
@@ -7,12 +7,9 @@ from .output import output as out
 
 def Execute(cursor, courseName):
     #and questions NOT LIKE '__' means that we needn't questions like {}
-    #and attempts < 4 means that we use only three first attempts, other attempts are not interesting
+    #and attempts < 4 means that we use only three first attempts, other attempts are not interesting    
     request = """
-        select 
-        course_name, problem_id,
-        attempts, questions,
-        page, grade, max_grade
+        select course_name, page, problem_type, problem_id, attempts, questions, grade, max_grade
         from problem_check
         where questions NOT LIKE '__'
         and attempts < 4
@@ -26,34 +23,35 @@ def Execute(cursor, courseName):
             columns_names.append(i[0])
             
         df = pd.DataFrame(data=data, columns=columns_names)
+        
         #changing refer link and cut problem_id
-        df['page']= df['page'].apply(lambda x: x.split('/handler')[0])
-        df['problem_id']= df['problem_id'].apply(lambda x: x.split('@')[-1])
+        df['page']= df['page'].apply(lambda x: x.split('?child')[0])        
+        df['problem_id']= df['problem_id'].apply(lambda x: x.split('@')[-1]) 
+        
+        tmpdf=df.groupby(['course_name','page','problem_type','problem_id','attempts'])['max_grade'].agg([np.max]) 
+        tmpdf=tmpdf.rename(columns={'amax': 'max_grade'})        
         
         #calculating: average grade, median of grade, number of answers for each test
         avg = ta.calculateTests(df)
+        
         #counting percent of right answers on each question
         #parsing questions string in two columns: question and result
-        listForQuestions=pd.DataFrame(columns=('course_name','problem_id','page','question','attempts','result','max_grade'))
-        for rowCounter in range(df.shape[0]):
-            currentAttempt=df.attempts[rowCounter]
-            currentPage=df.page[rowCounter]
-            currentCourse=df.course_name[rowCounter]
-            currentGrade=df.max_grade[rowCounter]
-            currentProblem=df.problem_id[rowCounter]
-            row=(df.questions[rowCounter][1:-1]).split(',')
-            for cellRow in row:
-                tmprow=cellRow.split(':')
-                if tmprow[1]=='True':
-                    listForQuestions=listForQuestions.append({'course_name':currentCourse,'problem_id':currentProblem,'page':currentPage,'question':tmprow[0],'attempts':currentAttempt,'result':1,'max_grade':currentGrade}, ignore_index=True)
-                else:
-                    listForQuestions=listForQuestions.append({'course_name':currentCourse,'problem_id':currentProblem,'page':currentPage,'question':tmprow[0],'attempts':currentAttempt,'result':0,'max_grade':currentGrade}, ignore_index=True)
-        listForQuestions['question'] = listForQuestions['question'].map(str.strip) #deleting spaces
-        listForQuestions['question']=listForQuestions['question'].apply(lambda x: ('"'+x.split('_')[-2] + '_' + x.split('_')[-1]))
+        
+        new_cols = df['questions'].str[1:-1]
+        new_cols=new_cols.str.split(',').apply(pd.Series, 1).stack()   
+        new_cols=new_cols.apply(lambda x: pd.Series(x.split(':')[-1]))         
+        new_cols=new_cols.reset_index(level=1)
+        new_cols=new_cols.rename(columns={'level_1': 'question', 0:'result'})
+        new_cols['question']=new_cols['question'].apply(lambda x: pd.Series(x+1))
+        new_cols['result'] = new_cols['result'].map({'True': 1, 'False': 0})
+        listForQuestions = pd.DataFrame(df, columns=['course_name','page','problem_type','problem_id','attempts'])
+        listForQuestions=listForQuestions.join(new_cols)
+
         perc = qa.calculateQuestions(listForQuestions)
         
         #merge results of analysis (average and percentade) into one table
-        res=pd.merge(avg, perc, on=['course_name','problem_id','page','attempts','max_grade'], how='outer')
+        res=pd.merge(avg, perc, on=['course_name','page','problem_type','problem_id','attempts'], how='outer')
+        res=pd.merge(res, tmpdf, on=['course_name','page','problem_type','problem_id','attempts'], how='outer')
     else:
         res=""
     return out.output(res, courseName)
